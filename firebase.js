38f84935a90155f5ed42c269ce1e59bd47ce30eb
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-06-13 21:16:51 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,21 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDeu4iivkbwlCe1prOw5vfk4Q-mKSbdCNc",
-    authDomain: "twitter-clone-96c9d.firebaseapp.com",
-    projectId: "twitter-clone-96c9d",
-    storageBucket: "twitter-clone-96c9d.appspot.com",
-    messagingSenderId: "176637391896",
-    appId: "1:176637391896:web:3da5008d14cbd69bc9ff6d"
+
+  apiKey: "AIzaSyD6e9M-4YKYdRMGSLNkH-hIHeKSW41PkH0",
+
+  authDomain: "twitter-build-e206a.firebaseapp.com",
+
+  projectId: "twitter-build-e206a",
+
+  storageBucket: "twitter-build-e206a.appspot.com",
+
+  messagingSenderId: "605592184837",
+
+  appId: "1:605592184837:web:3689d5b9aeb6a5412e946c"
+
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
